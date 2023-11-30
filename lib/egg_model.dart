dd0561960489e0ebbfa3ca296d9bacd17f38989f
Wojciech Skital <k3iran@gmail.com>
2023-11-30 09:30:20 +0100

    gotowa aplikacja

---
+++ lib/egg_model.dart
@@ -0,0 +1,12 @@
+enum EggSelection {
+  soft,
+  medium,
+  hard,
+}
+
+class EggModel {
+  final String hardnessTitle;
+  final double time;
+
+  EggModel(this.hardnessTitle, this.time);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_course/egg_model.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,113 +18,133 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        scaffoldBackgroundColor: Colors.amber,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  late String titleText;
 
-  void _incrementCounter() {
+  final EggModel soft = EggModel("Na miękko", 3.0);
+  final EggModel medium = EggModel("Na średnio", 5.0);
+  final EggModel hard = EggModel("Na twardo", 7.0);
+  double progressBar = 0.0;
+  double secondPass = 0.0;
+  final AudioPlayer player = AudioPlayer();
+
+  @override
+  void initState() {
+    titleText = "Jakie jajko wariacie?";
+    super.initState();
+  }
+
+  void selectEgg(EggSelection select) {
+    secondPass = 0.0;
+    progressBar = 0.0;
+    switch (select) {
+      case EggSelection.soft:
+        jajoAction(soft);
+      case EggSelection.medium:
+        jajoAction(medium);
+      case EggSelection.hard:
+        jajoAction(hard);
+    }
+  }
+
+  void jajoAction(EggModel eggModel){
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      titleText = eggModel.hardnessTitle;
+    });
+    var startTime = eggModel.time;
+    Timer.periodic(Duration(seconds: 1), (timer) {
+      secondPass++;
+      setState(() {
+        progressBar = secondPass / startTime;
+      });
+
+      if (secondPass >= eggModel.time) {
+        timer.cancel();
+        setState(() {
+          titleText = 'Ugotowane';
+        });
+        player.play(AssetSource("alarm_sound.mp3"));
+      }
     });
   }
 
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              titleText,
+              style: GoogleFonts.lato(
+                fontSize: 24,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                children: [
+                  Jajo("assets/soft_egg@3x.png", () => selectEgg(EggSelection.soft) ),
+                  Jajo("assets/medium_egg@3x.png", () => selectEgg(EggSelection.medium)),
+                  Jajo("assets/hard_egg@3x.png", () => selectEgg(EggSelection.hard)),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25, vertical: 30),
+              child: LinearProgressIndicator(
+                value: progressBar,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Jajo extends StatelessWidget {
+  const Jajo(
+    this.asset,
+    this.onTap, {
+    super.key,
+  });
+  final String asset;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: GestureDetector(
+          onTap: onTap,
+          child: Image.asset(
+            asset,
+            fit: BoxFit.contain,
+          ),
+        ),
+      ),
     );
   }
 }
